785eba1c870b915d2b58d8f9dc4a9be0a0d7fe55
ebears <thomlbmclean@gmail.com>
2022-11-04 19:55:29 -0400

    Second Quest (WIP)

--- include/constants/quests.h
+++ include/constants/quests.h
@@ -16,7 +16,7 @@
 
 // quest number defines
 #define QUEST_RIVAL_BATTLE          0
-#define QUEST_2          1
+#define QUEST_WALLYS_FIRST_POKEMON  1
 #define QUEST_3          2
 #define QUEST_4          3
 #define QUEST_5          4
@@ -78,9 +78,8 @@
 #define SUB_QUEST_29        28
 #define SUB_QUEST_30        29
 
-#define QUEST_1_SUB_COUNT 10
 #define QUEST_2_SUB_COUNT 20
-#define SUB_QUEST_COUNT (QUEST_1_SUB_COUNT + QUEST_2_SUB_COUNT)
+#define SUB_QUEST_COUNT (QUEST_2_SUB_COUNT)
 
 #define QUEST_ARRAY_COUNT (SUB_QUEST_COUNT > QUEST_COUNT ? SUB_QUEST_COUNT : QUEST_COUNT)
 #endif // GUARD_CONSTANTS_QUESTS_H
